ad13b9874b75171d168a06b2b0bc292c56241414
matias <manevimatias@gmail.com>
2022-11-01 02:34:02 -0300

    commit1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DetalleProductoComponent } from './productos/detalle-producto.component';
+import { EditarProductoComponent } from './productos/editar-producto.component';
+import { ListaProductoComponent } from './productos/lista-producto.component';
+import { NuevoProductoComponent } from './productos/nuevo-producto.component';
+import { LoginComponent } from './auth/login.component';
 
-const routes: Routes = [];
+import { GuardService as guard} from './guards/guard.service';
+import { AdminComponent } from './users/admin.component';
+import { UserComponent } from './users/user.component';
+import { RegistroComponent } from './users/registro.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+
+  //productos: utilizamos canActivate
+  {path: 'detalle/:id', component: DetalleProductoComponent, canActivate: [guard], data: { expectedRol: ['admin', 'user']}},
+  {path: 'nuevo', component: NuevoProductoComponent, canActivate: [guard], data: { expectedRol: ['admin']}},
+  {path: 'editar/:id', component: EditarProductoComponent, canActivate: [guard], data: { expectedRol: ['admin']}},
+
+  // user y admin
+  {path: 'admin', component: AdminComponent, canActivate: [guard], data: {expectedRol: ['admin']}},
+  {path: 'user', component: UserComponent, canActivate: [guard], data: {expectedRol: ['user']}},
+
+  // rutas a login y registro
+  {path: 'login', component: LoginComponent},
+  {path: 'registro', component: RegistroComponent},
+  {path: '**', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// modulos para el cliente http y los formularios
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ListaProductoComponent } from './productos/lista-producto.component';
+import { HomeComponent } from './home/home.component';
+import { DetalleProductoComponent } from './productos/detalle-producto.component';
+import { NuevoProductoComponent } from './productos/nuevo-producto.component';
+import { EditarProductoComponent } from './productos/editar-producto.component';
+import { LoginComponent } from './auth/login.component';
+import { interceptorProvider } from './interceptors/producto-interceptor.service';
+import { UserComponent } from './users/user.component';
+import { AdminComponent } from './users/admin.component';
+import { RegistroComponent } from './users/registro.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaProductoComponent,
+    HomeComponent,
+    DetalleProductoComponent,
+    NuevoProductoComponent,
+    EditarProductoComponent,
+    LoginComponent,
+    UserComponent,
+    AdminComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [interceptorProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
